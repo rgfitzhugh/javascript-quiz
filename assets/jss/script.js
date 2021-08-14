6a5bf726a6c5b742e77588798aa8c6a19b34feaf
Robert Fitzhugh <rgfitzhugh@gmail.com>
2021-08-14 01:36:50 -0600

    initial html, questions

---
+++ assets/jss/script.js
@@ -0,0 +1,94 @@
+//Quiz Questions
+const questions = [
+    {
+        q: "A built-in Javascript function that will push messages to the brower's console? It's used for debugging and seeing how variables are changing the code.",
+        a: [
+            {text: ".floor()", correct: false},
+            {text: "console.log()", correct: true},
+            {text: ".ceil()", correct: false},
+            {text: ".sqrt()", correct: false},
+        ]
+    },
+    {
+        q: 'What must a variable begin with?',
+        a: [
+            {text: "Container for a piece of data", correct: false},
+            {text: "A letter, $, or _", correct: true},
+            {text: "Local or global'", correct: false},
+            {text: "Undefined", correct: false},
+        ]
+    },
+
+    {
+        q: 'A javascript function that writes a string in the HTML document',
+        a: [
+            {text: "decrement--", correct: false},
+            {text: "boolean", correct: false},
+            {text: "document.write()", correct: true},
+            {text: "increment++", correct: false},
+        ]
+    },
+    {
+        q: "Syntax that returns true, if the operand is false, and false if the operand is true?",
+        a: [
+            {text: "!", correct: true},
+            {text: "&&", correct: false},
+            {text: "variable", correct: false},
+            {text: "||", correct: false},
+        ]
+    },
+    {
+        q: "Which operator subtracts the numeric value of its operand by one?",
+        a: [
+            {text: "document.write", correct: false},
+            {text: "decrement--", correct: true},
+            {text: "concatenation", correct: false},
+            {text: "increment++", correct: false},
+        ]
+    },
+    {
+        q: 'A container for storing data values. Declared by the keyword "var"?',
+        a: [
+            {text: "div", correct: false},
+            {text: "string", correct: false},
+            {text: "array", correct: false},
+            {text: "variable", correct: true},
+        ]
+    },
+    {
+        q: 'Syntax that returns true, if both opersnds are true?',
+        a: [
+            {text: "&&", correct: true},
+            {text: "!", correct: false},
+            {text: "||", correct: false},
+            {text: "++", correct: false},
+        ]
+    },
+    {
+        q: 'A programming language that add interactivity to the web?',
+        a: [
+            {text: "variable", correct: false},
+            {text: "decrement--", correct: false},
+            {text: "increment++", correct: false},
+            {text: "Javascript", correct: true},
+        ]
+    },
+    {
+        q: 'What data type only has two possible values (true and false)?',
+        a: [
+            {text: "modulus", correct: false},
+            {text: "boolean", correct: true},
+            {text: "string", correct: false},
+            {text: "array", correct: false},
+        ]
+    },
+    {
+        q: 'What stores values into a variable? Represented by "=".',
+        a: [
+            {text: "heavy equipment operator", correct: false},
+            {text: "switchboard operator", correct: false},
+            {text: "assignment operator", correct: true},
+            {text: "comparison operator", correct: false},
+        ]
+    },
+]
