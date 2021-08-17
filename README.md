cbe26fc9ff5f81f0028ca7a85ea4537e014c504f
Robert Fitzhugh <rgfitzhugh@gmail.com>
2021-08-16 19:13:32 -0600

    finished code (after git issues)

---
+++ README.md
@@ -0,0 +1,22 @@
+# Javascript Quiz
+
+This is the week 4 challenge of bootcamp
+
+This week's challenge was to create a javascript quiz using javascript. html, and css.
+
+This quiz consists of 10 questions.  The quiztaker is given 60 seconds to answer all 10 questions.
+
+When the timer equals 0 or all the questions are answered the quiz will end and the score is revealed
+
+After the quiz, the quiztaker will be able to save their initals along with their quiz score.
+
+
+These files for this project can be found in the following location:
+
+* [GitHub Repsoitory](https://github.com/rgfitzhugh/javascript-quiz)
+
+To access the password generator directly visit
+
+* [Password Generator Url](https://rgfitzhugh.github.io/javascript-quiz/)
+
+![quiz screenshot](assets/media.capture.png)
--- assets/jss/script.js
+++ assets/jss/script.js
@@ -1,7 +1,27 @@
+//Assignments
+const startTextEl = document.getElementById("start-text");
+const startTitleEL = document.getElementById("start-title")
+const beginQuiz = document.getElementById("begin-quiz");
+const timerEl = document.getElementById("timer");
+const questionContainerEl = document.getElementById("question-container");
+const questionEl = document.getElementById("question");
+const answerButtonsEl = document.getElementById("answer-buttons");
+const nextButtonEl = document.getElementById('next-button');
+const viewScoresEl = document.getElementById('view-scores');
+const enterScoreButtonEl = document.getElementById('enter-score-button');
+const endGameButton = document.getElementById("endgame-button");
+const highScoreContainer = document.getElementById('hs-container');
+const inputScoreEl = document.getElementById("hs-chart");
+const saveButtonEl = document.getElementById("save-button");
+const highScoreChartEl = document.getElementById("high-score-chart");
+var scoreCounter = 0;
+
+let shuffledQuestions, currentQuestionIndex
+
 //Quiz Questions
 const questions = [
     {
-        q: "A built-in Javascript function that will push messages to the brower's console? It's used for debugging and seeing how variables are changing the code.",
+        q: "A built-in Javascript function that will log information in the browser's console?",
         a: [
             {text: ".floor()", correct: false},
             {text: "console.log()", correct: true},
@@ -92,3 +112,180 @@ const questions = [
         ]
     },
 ]
+
+//Event Listeners
+beginQuiz.addEventListener("click", startGame);
+nextButtonEl.addEventListener("click", ()=> {
+    currentQuestionIndex++
+    setNextQuestion();
+})
+viewScoresEl.addEventListener("click", seeScore)
+enterScoreButtonEl.addEventListener("click", viewHighScore)
+endGameButton.addEventListener("click", seeScore)
+
+//Save Button Event Listener
+saveButtonEl.addEventListener('click',function(event){
+    event.preventDefault();
+   
+    var scoreInput = document.querySelector('#high-score').value + ": " + scoreCounter;
+
+    if (scoreInput === '') {
+        alert("Please type your initials!");
+    };
+        localStorage.setItem('score', scoreInput);
+    
+    renderLastScoreInput();
+
+})
+
+//Timer Display Message
+function displayMessage(){
+    document.getElementById('timer').innerHTML="Quiz Over!";
+}
+
+//Quiz Timer
+var timeLeft = 60;
+
+function countdown() {
+    var timeInterval = setInterval(function() {
+        document.getElementById("timer").innerHTML= ":" + timeLeft;
+        timeLeft--;
+        if (timeLeft < 1) {
+            clearInterval(timeInterval);
+            displayMessage();
+            viewScoresEl.classList.add('hide')
+            questionContainerEl.classList.add('hide')
+            enterScoreButtonEl.classList.remove('hide')
+            endGameButton.classList.add('hide')
+            highScoreContainer.classList.remove('hide')
+            seeScore();
+        }
+    }, 1000);
+}
+
+
+//Start Game
+function startGame() {
+//hide Start Quiz elements
+    beginQuiz.classList.add('hide')
+    startTextEl.classList.add('hide')
+    startTitleEL.classList.add('hide')
+//Shuffle Question Order
+    shuffledQuestions = questions.sort(()=> Math.random() - .5)
+    currentQuestionIndex = 0
+//Recall Quiz Questions
+    questionContainerEl.classList.remove('hide')
+//Recall Buttons
+   
+    nextButtonEl.classList.remove('hide')
+
+    setNextQuestion();
+    countdown();
+    scoreCounter = 0;
+}
+
+//Shows Questions on Screen
+function showQuestion (questions) {
+    questionEl.innerText = questions.q
+    questions.a.forEach(a => {
+        const button = document.createElement('button')
+        button.innerText = a.text
+        button.classList.add('btn')
+        if (a.correct) {
+            button.dataset.correct = a.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+}
+
+//Next Question
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+//Reset State of Questions
+function resetState() {
+    nextButtonEl.classList.add('hide')
+    highScoreContainer.classList.add('hide')
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
+//Select Answer
+function selectAnswer (e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1){
+    nextButtonEl.classList.remove('hide')
+    } else {
+        endGameButton.classList.remove('hide')
+        }
+    
+    if (selectedButton.dataset = correct){
+        scoreCounter++;
+    } else {
+        timeLeft-=10;
+    }
+}
+//Sets the correct answers to be correct
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove ('correct')
+    element.classList.remove('wrong')
+}
+
+
+//See Last High Score
+function renderLastScoreInput(){
+    highScoreChartEl .classList.remove("hide");
+    var scoreInput = localStorage.getItem("score");
+    highScoreChartEl.textContent = scoreInput;
+
+}
+
+//End Game 
+function endGame(){
+    seeScore();
+};
+
+//See Score
+function seeScore (){
+    questionContainerEl.classList.add('hide')
+    viewScoresEl.classList.add('hide')
+    enterScoreButtonEl.classList.remove('hide')
+    nextButtonEl.classList.add('hide')
+    highScoreContainer.classList.remove('hide')
+    function hsMessage(){
+        document.getElementById('hs-container').innerHTML="Your Score: " + scoreCounter;
+        
+        }
+    timeLeft=1;
+    hsMessage();
+}
+
+//View High Score
+function viewHighScore (){
+    questionContainerEl.classList.add('hide')
+    viewScoresEl.classList.add('hide')
+    enterScoreButtonEl.classList.add('hide')
+    nextButtonEl.classList.add('hide')
+    highScoreContainer.classList.remove('hide')
+    inputScoreEl.classList.remove('hide')
+
+    renderLastScoreInput();
+}
